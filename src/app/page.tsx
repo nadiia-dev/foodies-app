37a559f817e69285a736076754be4ee9da1e8465
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-03-18 15:40:15 +0200

    add main routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Page</div>;
+  return (
+    <div>
+      <h1 className="text-center text-white font-bold text-2xl">
+        Let`s get started!
+      </h1>
+      <p>
+        <Link href="/meals">Meals</Link>
+        <Link href="/meals/share">Share Meals</Link>
+        <Link href="/community">Community</Link>
+      </p>
+    </div>
+  );
 }
