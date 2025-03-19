00d875083814278fc8cb2fefc4fb2fc7b7875c7e
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-03-19 13:07:06 +0200

    add image picker preview

--- src/components/meals/image-picker.tsx
+++ src/components/meals/image-picker.tsx
@@ -1,16 +1,41 @@
 "use client";
-import { useRef } from "react";
+import Image from "next/image";
+import { useRef, useState } from "react";
 
 const ImagePicker = ({ label, name }: { label: string; name: string }) => {
+  const [pickedImage, setPickedImage] = useState(null);
+
   const imageInput = useRef<HTMLInputElement>(null);
   const handlePickClick = () => {
     imageInput.current?.click();
+  };
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
   };
 
   return (
     <div>
       <label htmlFor={name}>{label}</label>
       <div className="flex items-start gap-6 mb-4">
+        <div className="w-40 h-40 border-2 border-[#a4abb9] flex justify-center items-center text-center text-[#a4abb9] relative">
+          {!pickedImage && <p className="m-0 p-4">No image picked yet.</p>}
+          {pickedImage && (
+            <Image
+              src={pickedImage}
+              className="object-cover"
+              alt="The image selected by the user."
+              fill
+            />
+          )}
+        </div>
         <input
           className="hidden"
           type="file"
@@ -18,6 +43,7 @@ const ImagePicker = ({ label, name }: { label: string; name: string }) => {
           accept="image/png, image/jpeg"
           name={name}
           ref={imageInput}
+          onChange={handleImageChange}
         />
         <button
           className="border-0 py-2 px-6 bg-[#a4abb9] rounded cursor-pointer font-inherit hover:bg-[#b3b9c6] focus:outline-none"
